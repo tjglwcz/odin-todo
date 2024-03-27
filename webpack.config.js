125c6986ff116d4b06bfbafa11df283b5d0e52f3
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-03-27 17:57:53 +0100

    Enable Webpack and Bulma framework

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+};
