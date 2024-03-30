da3714c9c8ac648164c9b3bf44eaa81cefa6c4a0
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-03-30 07:59:52 +0100

    Add createProject function

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,20 @@ import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
 
 console.log("Hello World");
+
+let projects = [];
+
+function createProject(projectName) {
+  return {
+    projectName: projectName,
+    tasks: [],
+    addTask(task) {
+      this.tasks.push(task);
+    },
+  };
+}
+
+let project1 = createProject("siema");
+
+project1.addTask("task1");
+console.log(project1.tasks[0]);
