5994379651f83a1109b2d39d65145ab9960b38c7
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-03-30 08:52:52 +0100

    Add a function handling task creation

--- src/js/index.js
+++ src/js/index.js
@@ -26,3 +26,33 @@ project1.projectTasks.push("siema");
 
 console.log(project1.projectTasks);
 console.log(projects);
+
+function taskHandler() {
+  function addTask(title, description, dueDate, project, priority) {
+    const newTask = {
+      taskTitle: title,
+      taskDescription: description,
+      taskDueDate: dueDate,
+      taskProject: project,
+      taskPriority: priority,
+    };
+    const targetProject = projects.find(
+      (proj) => proj.projectName === newTask.taskProject
+    );
+    targetProject.projectTasks.push(newTask);
+    return newTask;
+  }
+
+  return { addTask };
+}
+
+const tasks = taskHandler();
+
+let task1 = tasks.addTask(
+  "Task 1",
+  "This is a description.",
+  "2023-03-30",
+  "Project 1",
+  3
+);
+console.log(projects);
