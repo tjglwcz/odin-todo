0fe5d323057e213b735cf2a887793711cf735a0f
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-03-30 08:32:14 +0100

    Add a function handling project creation

--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,27 @@ import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
 
 console.log("Hello World");
+
+const projects = [];
+
+function projectHandler() {
+  function createProject(projectName) {
+    const newProject = {
+      projectName: projectName,
+      projectTasks: [],
+    };
+    projects.push(newProject);
+    return newProject;
+  }
+
+  return { createProject };
+}
+
+const projHandler = projectHandler();
+
+let project1 = projHandler.createProject("Project 1");
+
+project1.projectTasks.push("siema");
+
+console.log(project1.projectTasks);
+console.log(projects);
