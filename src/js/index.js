738df2f175ac5363147879cdf9d34265c104de80
tjglwcz <117677164+tjglwcz@users.noreply.github.com>
2024-04-10 13:14:20 +0200

    Test Project and Task modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,156 +1,171 @@
 import "../scss/styles.scss";
 import * as bootstrap from "bootstrap";
+import { Project } from "./project";
+import { Task } from "./task";
 
 const taskContainer = document.querySelector("#taskContainer");
 const addToDoBtn = document.querySelector("#saveToDo");
 
 const projects = [];
 
-function projectHandler() {
-  function createProject(projectName) {
-    const newProject = {
-      projectName: projectName,
-      projectTasks: [],
-    };
-    projects.push(newProject);
-    return newProject;
-  }
+let newProject = new Project("First Project");
+newProject.add(projects);
 
-  return { createProject };
-}
+let newTask = new Task(
+  "Task Title",
+  "This is a description.",
+  "2024-04-10",
+  "First Project"
+);
+newTask.add(projects);
 
-const projHandler = projectHandler();
+console.log(projects);
 
-let project1 = projHandler.createProject("Project 1");
+// function projectHandler() {
+//   function createProject(projectName) {
+//     const newProject = {
+//       projectName: projectName,
+//       projectTasks: [],
+//     };
+//     projects.push(newProject);
+//     return newProject;
+//   }
 
-function taskHandler() {
-  function addTask(title, description, dueDate, project, priority) {
-    const newTask = {
-      taskTitle: title,
-      taskDescription: description,
-      taskDueDate: dueDate,
-      taskProject: project,
-      taskPriority: priority,
-    };
-    const targetProject = projects.find(
-      (proj) => proj.projectName === newTask.taskProject
-    );
-    targetProject.projectTasks.push(newTask);
-    return newTask;
-  }
+//   return { createProject };
+// }
 
-  return { addTask };
-}
+// const projHandler = projectHandler();
 
-const tasks = taskHandler();
+// let project1 = projHandler.createProject("Project 1");
 
-for (let i = 0; i < 5; i++) {
-  tasks.addTask(
-    `Task ${i + 1}`,
-    "This is a description.",
-    "2023-03-30",
-    "Project 1",
-    3
-  );
-}
+// function taskHandler() {
+//   function addTask(title, description, dueDate, project, priority) {
+//     const newTask = {
+//       taskTitle: title,
+//       taskDescription: description,
+//       taskDueDate: dueDate,
+//       taskProject: project,
+//       taskPriority: priority,
+//     };
+//     const targetProject = projects.find(
+//       (proj) => proj.projectName === newTask.taskProject
+//     );
+//     targetProject.projectTasks.push(newTask);
+//     return newTask;
+//   }
 
-function addCard(title, description, duedate, project) {
-  const card = document.createElement("div");
-  card.className = "card my-2";
+//   return { addTask };
+// }
 
-  const cardBody = document.createElement("div");
-  cardBody.className = "card-body";
+// const tasks = taskHandler();
 
-  const cardTop = document.createElement("div");
-  cardTop.className = "card-top d-flex gap-3";
+// for (let i = 0; i < 5; i++) {
+//   tasks.addTask(
+//     `Task ${i + 1}`,
+//     "This is a description.",
+//     "2023-03-30",
+//     "Project 1",
+//     3
+//   );
+// }
 
-  const cardCheckbox = document.createElement("input");
-  cardCheckbox.className = "form-check-input";
-  cardCheckbox.setAttribute("type", "checkbox");
+// function addCard(title, description, duedate, project) {
+//   const card = document.createElement("div");
+//   card.className = "card my-2";
 
-  const cardTitle = document.createElement("h5");
-  cardTitle.className = "card-title";
+//   const cardBody = document.createElement("div");
+//   cardBody.className = "card-body";
 
-  const cardIcons = document.createElement("div");
-  cardIcons.className = "ms-auto d-flex gap-2";
+//   const cardTop = document.createElement("div");
+//   cardTop.className = "card-top d-flex gap-3";
 
-  const cardEditIcon = document.createElement("i");
-  cardEditIcon.className = "bi bi-pencil-square";
+//   const cardCheckbox = document.createElement("input");
+//   cardCheckbox.className = "form-check-input";
+//   cardCheckbox.setAttribute("type", "checkbox");
 
-  const cardDeleteIcon = document.createElement("i");
-  cardDeleteIcon.className = "bi bi-trash text-danger";
+//   const cardTitle = document.createElement("h5");
+//   cardTitle.className = "card-title";
 
-  const cardText = document.createElement("p");
-  cardText.className = "card-text";
+//   const cardIcons = document.createElement("div");
+//   cardIcons.className = "ms-auto d-flex gap-2";
 
-  const cardFooter = document.createElement("div");
-  cardFooter.className = "card-footer d-flex justify-content-between";
+//   const cardEditIcon = document.createElement("i");
+//   cardEditIcon.className = "bi bi-pencil-square";
 
-  const cardDueDate = document.createElement("small");
-  const cardProject = document.createElement("small");
+//   const cardDeleteIcon = document.createElement("i");
+//   cardDeleteIcon.className = "bi bi-trash text-danger";
 
-  card.appendChild(cardBody);
+//   const cardText = document.createElement("p");
+//   cardText.className = "card-text";
 
-  cardBody.appendChild(cardTop);
+//   const cardFooter = document.createElement("div");
+//   cardFooter.className = "card-footer d-flex justify-content-between";
 
-  cardTop.appendChild(cardCheckbox);
-  cardTop.appendChild(cardTitle);
-  cardTop.appendChild(cardIcons);
+//   const cardDueDate = document.createElement("small");
+//   const cardProject = document.createElement("small");
 
-  cardIcons.appendChild(cardEditIcon);
-  cardIcons.appendChild(cardDeleteIcon);
+//   card.appendChild(cardBody);
 
-  cardBody.appendChild(cardText);
-  card.appendChild(cardFooter);
-  cardFooter.appendChild(cardDueDate);
-  cardFooter.appendChild(cardProject);
+//   cardBody.appendChild(cardTop);
 
-  cardTitle.textContent = title;
-  cardText.textContent = description;
-  cardDueDate.textContent = `Due: ${duedate}`;
-  cardProject.textContent = `Project: ${project}`;
+//   cardTop.appendChild(cardCheckbox);
+//   cardTop.appendChild(cardTitle);
+//   cardTop.appendChild(cardIcons);
 
-  cardEditIcon.setAttribute("data-bs-target", "#exampleModalCenter");
-  cardEditIcon.setAttribute("data-bs-toggle", "modal");
+//   cardIcons.appendChild(cardEditIcon);
+//   cardIcons.appendChild(cardDeleteIcon);
 
-  cardEditIcon.addEventListener("click", () => {
-    let titleInput = document.getElementById("titleInput");
-    let descriptionInput = document.getElementById("descriptionInput");
-    let dateInput = document.getElementById("dateInput");
-    let projectInput = document.getElementById("projectInput");
+//   cardBody.appendChild(cardText);
+//   card.appendChild(cardFooter);
+//   cardFooter.appendChild(cardDueDate);
+//   cardFooter.appendChild(cardProject);
 
-    titleInput.value = title;
-    descriptionInput.value = description;
-    dateInput.value = duedate;
-    projectInput.value = project;
-  });
+//   cardTitle.textContent = title;
+//   cardText.textContent = description;
+//   cardDueDate.textContent = `Due: ${duedate}`;
+//   cardProject.textContent = `Project: ${project}`;
 
-  taskContainer.appendChild(card);
-}
+//   cardEditIcon.setAttribute("data-bs-target", "#exampleModalCenter");
+//   cardEditIcon.setAttribute("data-bs-toggle", "modal");
 
-addToDoBtn.addEventListener("click", () => {
-  let title = document.getElementById("titleInput");
-  let description = document.getElementById("descriptionInput");
-  let date = document.getElementById("dateInput");
-  let project = document.getElementById("projectInput");
-  addCard(title.value, description.value, date.value, project.value);
-  title.value = "";
-  description.value = "";
-  date.value = "";
-  project.value = "";
-});
+//   cardEditIcon.addEventListener("click", () => {
+//     let titleInput = document.getElementById("titleInput");
+//     let descriptionInput = document.getElementById("descriptionInput");
+//     let dateInput = document.getElementById("dateInput");
+//     let projectInput = document.getElementById("projectInput");
 
-function renderAllTasks(projects) {
-  for (let project in projects) {
-    for (let task of projects[project].projectTasks) {
-      addCard(
-        task.taskTitle,
-        task.taskDescription,
-        task.taskDueDate,
-        task.taskProject
-      );
-    }
-  }
-}
+//     titleInput.value = title;
+//     descriptionInput.value = description;
+//     dateInput.value = duedate;
+//     projectInput.value = project;
+//   });
 
-renderAllTasks(projects);
+//   taskContainer.appendChild(card);
+// }
+
+// addToDoBtn.addEventListener("click", () => {
+//   let title = document.getElementById("titleInput");
+//   let description = document.getElementById("descriptionInput");
+//   let date = document.getElementById("dateInput");
+//   let project = document.getElementById("projectInput");
+//   addCard(title.value, description.value, date.value, project.value);
+//   title.value = "";
+//   description.value = "";
+//   date.value = "";
+//   project.value = "";
+// });
+
+// function renderAllTasks(projects) {
+//   for (let project in projects) {
+//     for (let task of projects[project].projectTasks) {
+//       addCard(
+//         task.taskTitle,
+//         task.taskDescription,
+//         task.taskDueDate,
+//         task.taskProject
+//       );
+//     }
+//   }
+// }
+
+// renderAllTasks(projects);
